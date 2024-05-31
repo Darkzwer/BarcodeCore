a3202393f859a879fb5e87c4c20a0745fa8b1350
Darkzwer123 <137776351+Darkzwer123@users.noreply.github.com>
2024-05-31 16:58:10 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,20 +9,9 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "BarcodeCore",
-            targets: ["BarcodeCore"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+            targets: ["BarcodeCoreTarget"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "BarcodeCore",
-            dependencies: []),
-        .testTarget(
-            name: "BarcodeCoreTests",
-            dependencies: ["BarcodeCore"]),
+        .binaryTarget(name: "BarcodeCoreTarget", url: "https://pods.regulaforensics.com/Barcode/7.2.9105/DocumentReaderCore_barcode_7.2.9105.zip", checksum: "79bf65a744fd5e34fcf346737ed8e7036b7dde42d236dbb8055ee2ab9d1c9f3e")
     ]
 )
